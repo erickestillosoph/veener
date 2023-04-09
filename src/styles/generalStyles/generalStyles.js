1c842d6184840bea0d5d5b0c548e5b4d8a3e1589
LAPTOP-BRB2KDT4\erick <erick@yssp.ph>
2023-04-09 20:51:03 +0800

    0004 - Add general styles

---
+++ src/styles/generalStyles/generalStyles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+
+// Padding    
+    padding8: {
+      padding: 8,
+    }, 
+    padding16: {
+      padding: 16,
+    },
+    padding20: {
+      padding: 20,
+    },
+    padding24: {
+      padding: 24,
+    },
+    padding28: {
+      padding: 28,
+    },
+    padding32: {
+      padding: 32,
+    },
+    
+  });
+  
+  export default styles;
