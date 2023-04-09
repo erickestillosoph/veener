c39c44c08eed2bf201ee7f76664ff4c8885a7477
dblanes <blanesdouglas@gmail.com>
2023-04-09 22:04:30 +0800

    0005 - Added margin, padding, justifyContent and flex styles

--- src/styles/generalStyles/generalStyles.js
+++ src/styles/generalStyles/generalStyles.js
@@ -3,26 +3,192 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
 
-// Padding    
-    padding8: {
+ // Padding    
+    padding8px: {
       padding: 8,
     }, 
-    padding16: {
+    padding16px: {
       padding: 16,
-    },
-    padding20: {
-      padding: 20,
-    },
-    padding24: {
+    }, 
+    padding24px: {
       padding: 24,
-    },
-    padding28: {
-      padding: 28,
-    },
-    padding32: {
+    }, 
+    padding32px: {
       padding: 32,
     },
     
+    // paddingTop    
+    paddingTop8px: {
+      paddingTop: 8,
+    }, 
+    paddingTop16px: {
+      paddingTop: 16,
+    }, 
+    paddingTop24px: {
+      paddingTop: 24,
+    }, 
+    paddingTop32px: {
+      paddingTop: 32,
+    },
+
+    // paddingRight    
+    paddingRight8px: {
+      paddingRight: 8,
+    }, 
+    paddingRight16px: {
+      paddingRight: 16,
+    }, 
+    paddingRight24px: {
+      paddingRight: 24,
+    }, 
+    paddingRight32px: {
+      paddingRight: 32,
+    },
+
+    // paddingBottom    
+    paddingBottom8px: {
+      paddingBottom: 8,
+    }, 
+    paddingBottom16px: {
+      paddingBottom: 16,
+    }, 
+    paddingBottom24px: {
+      paddingBottom: 24,
+    }, 
+    paddingBottom32px: {
+      paddingBottom: 32,
+    },
+
+  // paddingLeft
+    paddingLeft8px: {
+      paddingLeft: 8,
+    }, 
+    paddingLeft16px: {
+      paddingLeft: 16,
+    }, 
+    paddingLeft24px: {
+      paddingLeft: 24,
+    }, 
+    paddingLeft32px: {
+      paddingLeft: 32,
+    },
+
+      
+    
+// Margin    
+    margin8px: {
+      margin: 8,
+    }, 
+    margin16px: {
+      margin: 16,
+    }, 
+    margin24px: {
+      margin: 24,
+    }, 
+    margin32px: {
+      margin: 32,
+    },
+
+    // marginTop    
+    marginTop8px: {
+      marginTop: 8,
+    }, 
+    marginTop16px: {
+      marginTop: 16,
+    }, 
+    marginTop24px: {
+      marginTop: 24,
+    }, 
+    marginTop32px: {
+      marginTop: 32,
+    },
+
+    // marginRight    
+    marginRight8px: {
+      marginRight: 8,
+    }, 
+    marginRight16px: {
+      marginRight: 16,
+    }, 
+    marginRight24px: {
+      marginRight: 24,
+    }, 
+    marginRight32px: {
+      marginRight: 32,
+    },
+
+    // marginBottom    
+    marginBottom8px: {
+      marginBottom: 8,
+    }, 
+    marginBottom16px: {
+      marginBottom: 16,
+    }, 
+    marginBottom24px: {
+      marginBottom: 24,
+    }, 
+    marginBottom32px: {
+      marginBottom: 32,
+    },
+    // marginLeft    
+    marginLeft8px: {
+      marginLeft: 8,
+    }, 
+    marginLeft16px: {
+      marginLeft: 16,
+    }, 
+    marginLeft24px: {
+      marginLeft: 24,
+    }, 
+    marginLeft32px: {
+      marginLeft: 32,
+    },
+    
+    //justifyContent
+    justifyContentCenter: {
+      justifyContent: 'center'
+    },
+    justifyContentBetween: {
+      justifyContent: 'space-between'
+    },
+    justifyContentAround: {
+      justifyContent: 'space-around'
+    },
+    justifyContentStart: {
+      justifyContent: 'flex-start'
+    },
+    justifyContentEnd: {
+      justifyContent: 'flex-end'
+    },
+
+    //displayFlex
+    displayFlex: {
+      flex: displayFlex
+    },
+    flexDirectionRow: {
+      flexDirection: 'row'
+    },   
+    flexDirectionRowReverse: {
+      flexDirection: 'row-reverse'
+    },
+    flexDirectionColumn: {
+      flexDirection: 'column'
+    },
+    flexDirectionColumnReverse: {
+      flexDirection: 'column-reverse'
+    },
+
+    //width
+    maxWidth: {
+      width: 100
+    },
+    //height
+    maxHeight: {
+      height: 100
+    },
+    zIndex: {
+      zIndex: 100
+    }
   });
   
   export default styles;
