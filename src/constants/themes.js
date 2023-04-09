0aa71bc20139228fbf1860136a13b341200621cf
LAPTOP-BRB2KDT4\erick <erick@yssp.ph>
2023-04-10 00:15:31 +0800

    0006 - Added the Light and dark Color Theme on the constants file

---
+++ src/constants/themes.js
@@ -0,0 +1,27 @@
+const darkThemeColors = {
+    primary : '#243442',
+    bgColor : '#243442',
+    bgPanelColor : '#33495C',
+    fntColor : '#fff',
+    subTxtColor : '#969696'
+}
+
+const lightThemeColors = {
+    primary : '#243442',
+    bgColor : '#fff',
+    bgPanelColor : '#F6F6F6',
+    fntColor : '#243442',
+    subTxtColor : '#969696'
+}
+
+const FONTS = {
+    extraLight: "Poppins-ExtraLight",
+    light: "Poppins-Light",
+    regular: "Poppins-Regular",
+    medium: "Poppins-Medium", 
+    semiBold: "Poppins-SemiBold", 
+    bold: "Poppins-Bold", 
+    extraBold: "Poppins-ExtraBold"
+}
+
+export default {darkThemeColors, lightThemeColors, FONTS}
